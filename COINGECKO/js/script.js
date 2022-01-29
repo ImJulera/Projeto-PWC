e6a0ebceb6e94199d7609b0404498f98cde5b508
Julera <73439718+LordTheCheater@users.noreply.github.com>
2022-01-29 02:35:01 +0000

    Fix SearchBar
    Fix de printar várias vezes a mesma moeda na lista, caso essa mesma seja pesquisada.

--- COINGECKO/js/script.js
+++ COINGECKO/js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
 	var cloneCoin = $('.coin').clone();
-	$('.coin').remove();
+	$('.coinsList').empty(); // .html('');
 	
 	$.ajax({
 		method: "GET",
@@ -15,15 +15,48 @@ $(document).ready(function() {
 			
 				// Criar novo clone
 				var liCoin = cloneCoin.clone();
+
 				// Alterar no clone
 				$('.coinRANK', liCoin).text(result.market_cap_rank);
 				$('#coinIMG', liCoin).attr('src' ,result.image);
 				$('.coinNAME', liCoin).text(result.name);
 				$('.coinPRICE', liCoin).text(result.current_price + '$');
 				
-				
 				// Adicionar o clone à tabela original
 				$('.coinsList').append(liCoin);
+			})
+		})
+	
+		$('#btnSearch').on('click', function() {
+
+			$('.coinsList').empty(); // .html('');
+			var valuePesquisa = $('#search').val();
+	
+			//console.log(valuePesquisa.lenght);
+	
+			$.ajax({
+				method: "GET",
+				url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+			}).done(function(res){
+	
+				$.each(res, function(index, result){
+					
+					
+					if(valuePesquisa == result.name)
+					{
+						// Criar novo clone
+						var liCoin = cloneCoin.clone();
+	
+						// Alterar no clone
+						$('.coinRANK', liCoin).text(result.market_cap_rank);
+						$('#coinIMG', liCoin).attr('src' ,result.image);
+						$('.coinNAME', liCoin).text(result.name);
+						$('.coinPRICE', liCoin).text(result.current_price + '$');
+	
+						// Adicionar o clone à tabela original
+						$('.coinsList').append(liCoin);
+					}
+				}) 
 			})
 		})
 })
--- COINGECKO/js/searchBar.js
+++ COINGECKO/js/searchBar.js
@@ -1,33 +1,37 @@
 'use strict';
 
-$('#btnSearch').on('click', function() {
+$(document).ready(function() {
+	$('#btnSearch').on('click', function() {
 
-	var valuePesquisa = $('#search').val();
-	$('.coinsList').empty(); // .html('');
+		var cloneCoin = $('.coin').clone();
+		$('.coinsList').empty(); // .html('');
+		var valuePesquisa = $('#search').val();
 
-    console.log(valuePesquisa.lenght);
+		//console.log(valuePesquisa.lenght);
 
-	$.ajax({
-		method: "GET",
-		url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
-	}).done(function(res){
+		$.ajax({
+			method: "GET",
+			url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+		}).done(function(res){
 
-		$.each(res, function(index, result){
-            
-            // Criar novo clone
-			var liCoin = cloneCoin.clone();
+			$.each(res, function(index, result){
+				
+				
+				if(valuePesquisa == result.name)
+				{
+					// Criar novo clone
+					var liCoin = cloneCoin.clone();
 
-            if(valuePesquisa == result.name)
-            {
-                // Alterar no clone
-                $('.coinRANK', liCoin).text(result.market_cap_rank);
-			    $('#coinIMG', liCoin).attr('src' ,result.image);
-			    $('.coinNAME', liCoin).text(result.name);
-			    $('.coinPRICE', liCoin).text(result.current_price + '$');
+					// Alterar no clone
+					$('.coinRANK', liCoin).text(result.market_cap_rank);
+					$('#coinIMG', liCoin).attr('src' ,result.image);
+					$('.coinNAME', liCoin).text(result.name);
+					$('.coinPRICE', liCoin).text(result.current_price + '$');
 
-                // Adicionar o clone à tabela original
-			    $('.coinsList').append(liCoin);
-            }
-		}) 
+					// Adicionar o clone à tabela original
+					$('.coinsList').append(liCoin);
+				}
+			}) 
+		})
 	})
 })
