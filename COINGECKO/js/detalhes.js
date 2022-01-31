cf5c59d2d81251da1cfb6b3b086d1638e7795e07
Julera <73439718+LordTheCheater@users.noreply.github.com>
2022-01-31 15:04:41 +0000

    Search Bar Update
    Remodelação da barra de pesquisa para uma barra de pesquisa com sugestões.

--- COINGECKO/js/detalhes.js
+++ COINGECKO/js/detalhes.js
@@ -27,7 +27,42 @@ $(document).ready(function(){
 					$('.coin-TotalCoins').text(result.circulating_supply);
                     $('.coin-UpdateData').text(result.last_updated);
                 }
+				
+			})
 
+			$('#btnSearch').on('click', function() {
+
+				var valuePesquisa = $('#search').val();
+		
+				//console.log(valuePesquisa.lenght);
+		
+				$.ajax({
+					method: "GET",
+					url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+				}).done(function(res){
+		
+					$('.coinsList').empty(); // .html('');
+
+					$.each(res, function(index, result){
+						
+						if(valuePesquisa == result.name)
+						{
+							$('#coinLOGO').attr('src' ,result.image);
+							$('.coin-Name').text(result.name);
+							$('.coin-Rank').text(result.market_cap_rank);
+							$('.coin-ValorAtual').text(result.current_price + "€");
+							$('.coin-TotalUnidades').text(result.total_volume);
+							$('.coin-MarketChangePercentage24h').text(result.price_change_percentage_24h + "%"); 
+							$('.coin-Preco24h').text(result.price_change_24h + "€");
+							$('.coin-High24h').text(result.high_24h + "€");
+							$('.coin-Low24h').text(result.low_24h + "€");
+							$('.coin-TotalCoins').text(result.circulating_supply);
+							$('.coin-UpdateData').text(result.last_updated);
+
+							localStorage.setItem('detailsCoinName', valuePesquisa);
+						}
+					}) 
+				})
 			})
 		})
 })
--- COINGECKO/js/searchBar.js
+++ COINGECKO/js/searchBar.js
@@ -1,37 +1,65 @@
 'use strict';
 
 $(document).ready(function() {
-	$('#btnSearch').on('click', function() {
 
-		var cloneCoin = $('.coin').clone();
-		$('.coinsList').empty(); // .html('');
-		var valuePesquisa = $('#search').val();
+	const searchBar = document.querySelector('.searchInput');
+	const inputBox = searchBar.querySelector('input');
+	const suggBox = searchBar.querySelector('.searchSuggestions');
+	let suggestions = [];
 
-		//console.log(valuePesquisa.lenght);
+	$.ajax({
+		method: "GET",
+		url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+	}).done(function(res){
+		
+		$.each(res, function(index, result){
 
-		$.ajax({
-			method: "GET",
-			url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
-		}).done(function(res){
-
-			$.each(res, function(index, result){
-				
-				
-				if(valuePesquisa == result.name)
-				{
-					// Criar novo clone
-					var liCoin = cloneCoin.clone();
-
-					// Alterar no clone
-					$('.coinRANK', liCoin).text(result.market_cap_rank);
-					$('#coinIMG', liCoin).attr('src' ,result.image);
-					$('.coinNAME', liCoin).text(result.name);
-					$('.coinPRICE', liCoin).text(result.current_price + '$');
-
-					// Adicionar o clone à tabela original
-					$('.coinsList').append(liCoin);
-				}
-			}) 
+			suggestions.push(result.name);
 		})
 	})
+
+	inputBox.onkeyup = (e)=>{
+		let usarData = e.target.value;
+		let emptyArray = [];
+		if(usarData)
+		{
+			emptyArray = suggestions.filter((data)=>{
+				return data.toLocaleLowerCase().startsWith(usarData.toLocaleLowerCase());
+			});
+			emptyArray = emptyArray.map((data)=>{
+				return data = '<li>' + data + '</li>';
+			});
+			searchBar.classList.add('active');
+			showSuggestions(emptyArray);
+			let allList = suggBox.querySelectorAll('li');
+			for(let i = 0 ; i < allList.length ; i++)
+			{
+				allList[i].setAttribute('onclick', 'select(this)');
+			}
+		}else{
+			searchBar.classList.remove('active');
+		}
+	}
+
+	function select(element)
+	{
+		let selectUserData = element.textContent;
+		console.log(selectUserData);
+	}
+
+	function showSuggestions(list)
+	{
+		let listData;
+		let userValue;
+		if(!list.length)
+		{
+			userValue = inputBox.value;
+			listData = '<li>' + userValue + '</li>';
+		}
+		else
+		{
+			listData = list.join('');
+		}
+		suggBox.innerHTML = listData;
+	}
 })
